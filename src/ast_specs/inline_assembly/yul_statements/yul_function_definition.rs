e32d712094cea73a09b49d140aff6bf880846e32
PFAhard <pasha-908@list.ru>
2024-08-26 08:48:37 +0300

    push

--- src/ast_specs/inline_assembly/yul_statements/yul_function_definition.rs
+++ src/ast_specs/inline_assembly/yul_statements/yul_function_definition.rs
@@ -10,7 +10,7 @@ pub struct YulFunctionDefinition {
     pub name: String,
     #[serde(rename = "nativeSrc")]
     pub native_src: Option<String>,
-    pub parameters: Vec<YulTypedName>,
-    pub return_variables: Vec<YulTypedName>,
+    pub parameters: Option<Vec<YulTypedName>>,
+    pub return_variables: Option<Vec<YulTypedName>>,
     pub src: String,
 }
